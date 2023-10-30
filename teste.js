3475e486d7cd43fd69dcaa5394c1e00561ede598
Rayane Oliveira <oliver.batista.ray@gmail.com>
2023-10-30 17:19:36 -0300

    exercicios da aula 1

---
+++ teste.js
@@ -0,0 +1,18 @@
+const valor1 = 3;
+const valor2 = 5;
+
+const soma = valor1 + valor2;
+
+console.log(soma);
+
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
